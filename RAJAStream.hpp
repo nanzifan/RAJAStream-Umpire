0895d6b7a817393591840308c85d5c2e0ed56e6b
znan <znan@ncsu.edu>
2018-08-06 16:24:10 -0700

    test

--- RAJAStream.hpp
+++ RAJAStream.hpp
@@ -10,23 +10,23 @@
 #include <stdexcept>
 #include "RAJA/RAJA.hpp"
 
-#include "Stream.h"
+// #include "Stream.h"
 
 #define IMPLEMENTATION_STRING "RAJA"
 
-#ifdef RAJA_TARGET_CPU
-typedef RAJA::policy::indexset::ExecPolicy<
-        RAJA::seq_segit,
-        RAJA::omp_parallel_for_exec> policy;
-typedef RAJA::omp_reduce reduce_policy;
-#else
-// const size_t block_size = 128;
-// typedef RAJA::cuda_exec<block_size> policy;
-// typedef RAJA::cuda_reduce<block_size> reduce_policy;
-#endif
+// #ifdef RAJA_TARGET_CPU
+// typedef RAJA::policy::indexset::ExecPolicy<
+//         RAJA::seq_segit,
+//         RAJA::omp_parallel_for_exec> policy;
+// typedef RAJA::omp_reduce reduce_policy;
+// #else
+// // const size_t block_size = 128;
+// // typedef RAJA::cuda_exec<block_size> policy;
+// // typedef RAJA::cuda_reduce<block_size> reduce_policy;
+// #endif
 
-template <class T>
-class RAJAStream : public Stream<T>
+
+class RAJAStream
 {
   protected:
     // Size of arrays
@@ -35,14 +35,14 @@ class RAJAStream : public Stream<T>
     // Contains iteration space
     // RAJA::IndexSet index_set;
 
-    T* a;
-    T* b;
-    T* c;
+    double* a;
+    double* b;
+    double* c;
 
     // Device side pointers to arrays
-    T* d_a;
-    T* d_b;
-    T* d_c;
+    double* d_a;
+    double* d_b;
+    double* d_c;
 
   public:
 
@@ -50,13 +50,7 @@ class RAJAStream : public Stream<T>
     ~RAJAStream();
 
     virtual void copy() override;
-    virtual void add() override;
-    virtual void mul() override;
-    virtual void triad() override;
-    virtual T dot() override;
 
-    virtual void init_arrays(T initA, T initB, T initC) override;
-    virtual void read_arrays(
-            std::vector<T>& a, std::vector<T>& b, std::vector<T>& c) override;
+    virtual void init_arrays(double initA, double initB, double initC) override;
 };
 
