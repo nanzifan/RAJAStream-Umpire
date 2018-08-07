fa77659497114357a1afcd4ebf08af8d53ec7eff
znan <znan@ncsu.edu>
2018-08-06 17:04:43 -0700

    test

--- main.cpp
+++ main.cpp
@@ -6,49 +6,13 @@
 // source code
 
 #include <iostream>
-#include <vector>
-#include <numeric>
-#include <cmath>
-#include <limits>
-#include <chrono>
-#include <algorithm>
-#include <iomanip>
-#include <cstring>
 
 #define VERSION_STRING "3.3"
 
-#include "Stream.h"
-
-#if defined(CUDA)
-#include "CUDAStream.h"
-#elif defined(HIP)
-#include "HIPStream.h"
-#elif defined(HC)
-#include "HCStream.h"
-#elif defined(OCL)
-#include "OCLStream.h"
-#elif defined(USE_RAJA)
 #include "RAJAStream.hpp"
-#elif defined(KOKKOS)
-#include "KokkosStream.hpp"
-#elif defined(ACC)
-#include "ACCStream.h"
-#elif defined(SYCL)
-#include "SYCLStream.h"
-#elif defined(OMP)
-#include "OMPStream.h"
-#endif
 
 // Default size of 2^13
 unsigned int ARRAY_SIZE = 16;
-unsigned int num_times = 100;
-unsigned int deviceIndex = 0;
-bool use_float = false;
-bool triad_only = false;
-bool output_as_csv = false;
-std::string csv_separator = ",";
-
-
 
 int main(int argc, char *argv[])
 {
